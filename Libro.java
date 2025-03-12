2da581eecf6c703f01dc8e214a88876fc2b4683d
Juan Manuel Ochoa <juanm.ochoamu@autonoma.edu.co>
2025-03-12 17:28:51 -0500

    Se agrega nueva info

---
+++ Libro.java
@@ -0,0 +1,27 @@
+
+
+public class Libro {
+    private int id;
+    private String titulo;
+
+    public Libro(int id, String titulo) {
+        this.id = id;
+        this.titulo = titulo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+}
